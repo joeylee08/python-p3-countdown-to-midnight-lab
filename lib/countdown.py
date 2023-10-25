adb375a8c8d038372e420b29050ec30bb00e2f12
Joseph <josephylee72@gmail.com>
2023-10-25 10:56:21 -0700

    completed countdown to midnight

--- lib/countdown.py
+++ lib/countdown.py
@@ -7,9 +7,9 @@ def countdown(int):
     print("HAPPY NEW YEAR!")
 
 def countdown_with_sleep(int):
-    while int > 0:
-        time.sleep(1000)
+    while int > 0: 
         print(f"{int} SECOND(S)!")
         int -= 1
-    time.sleep(1000)
+        time.sleep(1)
+    time.sleep(1)
     print("HAPPY NEW YEAR!")
